d8e3e0a3c7484d4081c48272ea1d1d296934594e
Dillon Brock <mdillon1297@gmail.com>
2022-09-26 12:23:41 -0700

    wrapped book titles in BookList in Link that redirects to the corresponding book detail page

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom';
 import Author from '../author/Author';
 import './Book.css';
 
 function Book({
-  book: { title, total_pages: totalPages, rating, isbn, published_date: publishedDate, authors },
+  book: { book_id, title, total_pages: totalPages, rating, isbn, published_date: publishedDate, authors },
   showDetail = false,
 }) {
   return (
     <article className="book">
-      <h3>{title}</h3>
+      <Link to={`/books/${book_id}`}>
+        <h3>{title}</h3>
+      </Link>
       <address>
         {authors.map((author) => (
           <Author key={author.author_id} author={author} />
